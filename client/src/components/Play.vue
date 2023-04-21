7ce1a19a8ee879d04158b31383471c0075a13e6c
Nurd Galim <nurd_galim@mail.ru>
2023-04-22 01:14:22 +0700

    some changes init play func

--- client/src/components/Play.vue
+++ client/src/components/Play.vue
@@ -1,10 +1,24 @@
 <template>
   <div class="container">
-    <img class="play-button" src="../assets/play_button.svg">
+    <img class="play-button" src="../assets/play_button.svg" @click="play" />
   </div>
 </template>
 
 <script>
+export default {
+  name: "Play",
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    play() {
+      console.log(this.counter);
+      this.counter++;
+    },
+  },
+};
 </script>
 
 <style>
@@ -17,7 +31,8 @@
   justify-content: center;
   text-align: center;
 }
+
 .play-button {
-  max-width: 50%;
+  max-width: 30%;
 }
 </style>
