4f84ccd24a42073cefc4dfde82fa7adc7bdd82c5
Nurd Galim <nurd_galim@mail.ru>
2023-04-27 17:46:02 +0700

    some style changes to play component

--- client/src/components/Play.vue
+++ client/src/components/Play.vue
@@ -1,26 +1,42 @@
 <template>
-  <div class="container">
-    <img class="play-button" src="../assets/play_button.svg" @click="play" />
+  <div class="container play-element">
+    <transition name="fade">
+      <img
+        v-if="typeof wannaColors == 'number'"
+        class="play-button"
+        src="../assets/play_button.svg"
+        @click="play"
+      />
+    </transition>
+    <div class="selecting">
+      <p>Количество цветов</p>
+      <!-- TODO change input of coloors -->
+      <select v-model="wannaColors">
+        <option :value="null"></option>
+        <option v-for="index in 15" :value="index">
+          {{ index }}
+        </option>
+      </select>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapMutations, mapActions, mapGetters } from 'vuex'
+import { mapMutations, mapActions, mapGetters } from "vuex";
 export default {
   name: "Play",
   data() {
     return {
       counter: 0,
+      wannaColors: null,
     };
   },
   computed: {
-    ...mapGetters({mainCount: 'counts'}),
+    ...mapGetters({ mainCount: "counts" }),
   },
   methods: {
     play() {
-      // console.log(this.counter);
-      // this.counter++;
-      console.log(this.mainCount)
+      console.log(this.mainCount);
     },
   },
 };
@@ -29,5 +45,30 @@ export default {
 <style>
 .play-button {
   max-width: 30%;
+}
+
+.play-element {
+  flex-direction: column;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.selecting {
+  width: 50%;
+  position: absolute;
+  bottom: 20%;
+}
+
+.select-list {
+  overflow: auto;
+  height: 20%;
 }
 </style>
