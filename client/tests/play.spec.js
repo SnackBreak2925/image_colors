9da7813e8aef30631d1ce47d229d8fad1d992d8d
Nurd Galim <nurd_galim@mail.ru>
2023-06-04 14:37:04 +0700

    end js tests (tmp probably)

--- client/tests/play.spec.js
+++ client/tests/play.spec.js
@@ -1,54 +1,58 @@
-// import { mount, createLocalVue, shallowMount } from '@vue/test-utils'
-// import Vuex from 'vuex'
-// import Play from '@/components/Play.vue'
-// // import store from '@/store/mainStore.js'
+import { mount, createLocalVue, shallowMount } from '@vue/test-utils'
+import Vuex from 'vuex'
+import Play from '@/components/Play.vue'
+import BootstrapVue from 'bootstrap-vue'
 
-// const localVue = createLocalVue()
-// localVue.use(Vuex)
+const localVue = createLocalVue()
+localVue.use(Vuex)
+localVue.use(BootstrapVue)
 
-// describe('Компонент Play', () => {
-//   let getters
-//   let store
-//   let actions
-//   let state
-//   const file = {
-//     name: 'upload.test.jpg',
-//     size: 61416,
-//     type: 'image/jpeg',
-//     lastModified: 1685642683687,
-//     webkitRelativePath: '',
-//   }
+describe('Компонент Play', () => {
+  let getters
+  let store
+  let actions
+  let state
+  const file = {
+    name: 'upload.test.jpg',
+    size: 61416,
+    type: 'image/jpeg',
+    lastModified: 1685642683687,
+    webkitRelativePath: '',
+  }
 
-//   beforeEach(() => {
-//     getters = {
-//       getFile: () => file,
-//     }
-//     state = {
-//       file: file
-//     }
-//     actions = {
-//       sendFile: jest.fn(),
-//       setCountColors: jest.fn(),
-//     }
-//     store = new Vuex.Store({
-//       getters,
-//       actions,
-//       state
-//     })
-//   })
-//   const wrapper = shallowMount(Play, { localVue, store })
+  beforeEach(() => {
+    getters = {
+      getFile: () => file,
+    }
+    state = {
+      file: file
+    }
+    actions = {
+      sendFile: jest.fn(),
+      setCountColors: jest.fn(),
+    }
+    store = new Vuex.Store({
+      getters,
+      actions,
+      state
+    })
+  })
+  const wrapper = shallowMount(Play, { localVue, store })
 
-//   it("вызывает запись количества цветов в хранилище при выборе", () => {
-//     const input = wrapper.find('b-form-select')
-//     wrapper.setData({ wannaColors: 12 })
-//     input.element.value = 12
-//     input.trigger('change')
-//     expect(store.actions.setCountColors(wrapper.vm.wannaColors)).toHaveBeenCalled()
-//   })
+  it("вызывает запись количества цветов в хранилище при выборе (пустышка)", () => {
+    // console.log(wrapper.html())
+    expect(wrapper.html()).toContain("b-form-select")
+    //   const input = wrapper.find('b-form-select')
+    //   wrapper.setData({ wannaColors: 12 })
+    //   input.element.value = 12
+    //   input.trigger('change')
+    //   expect(store.actions.setCountColors(wrapper.vm.wannaColors)).toHaveBeenCalled()
+    // })
 
-//   // it('отображается кнопка старта когда загружен файл и ввыбрано количество цветов', () => {
-//   //   wrapper.setData({ wannaColors: 12 })
-//   //   const img = wrapper.find('img')
-//     // await wrapper.vm.$nextTick()
-//   // })
-// })
+    // it('отображается кнопка старта когда загружен файл и ввыбрано количество цветов', () => {
+    //   wrapper.setData({ wannaColors: 12 })
+    //   const img = wrapper.find('img')
+    // await wrapper.vm.$nextTick()
+    // })
+  })
+})
